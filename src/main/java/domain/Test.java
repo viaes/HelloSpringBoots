87b45991872d18f4e0c3d5b306d09a4d4d0a558e
xiong <congcong2333@foxmail.com>
2020-11-04 11:34:58 +0800

    第一次提交

---
+++ src/main/java/domain/Test.java
@@ -0,0 +1,11 @@
+package domain;
+
+/**
+ * @Author: Mr.xiong
+ * @Date: 20-11-4 上午11:32
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(" main 方法");
+    }
+}
