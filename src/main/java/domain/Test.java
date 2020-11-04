8dcfbe0918974d86e3453ea4dddae0dc8ad0d8b5
xiong <congcong2333@foxmail.com>
2020-11-04 11:51:04 +0800

    添加test方法

--- src/main/java/domain/Test.java
+++ src/main/java/domain/Test.java
@@ -8,4 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(" main 方法");
     }
+    public void test(){
+        System.out.println("测试方法");
+    }
 }
